9e582d508e8140eddb1acb72b1890ba41e7d939e
Tom Merrell <tom@merrelltom.com>
2019-08-29 11:38:29 +0100

    Style Credit Card Form

--- site/templates/online_payment.php
+++ site/templates/online_payment.php
@@ -17,18 +17,36 @@
  ?>
 
 <section id="online-payment-screen" class="screen online-payment-screen selected">
-    <form action="./online-payment-end-screen" method="post" id="payment-form">
-        <input id="cardholder-name" type="text">
-        <input id="cardholder-email" type="text">
+  <div class="screen-content">
+    <h2 class="screen-title large-text">Online Payment</h2>
+    <div class="answers">
+      <h3 class="subtitle">Your Order:</h3>
+      <ul>
+        <li>Trophy Size: <?= $size;?></li>
+        <li>Price: <?= $price;?></li>   
+      </ul>
+    </div> 
+    <form action="./online-payment-end-screen" method="post" id="payment-form" class="answers">
+        <h3 class="subtitle">Payment Details:</h3><br>
+        <div class="input-wrapper"> 
+          <input id="cardholder-name" type="text" class="payment-form-input" placeholder="Carholder's Name">
+        </div>
+        
+        <div class="input-wrapper">
+          <input id="cardholder-email" type="text" class="payment-form-input" placeholder="Email Address">
+        </div>
+
         <input name="id" type="hidden" value="<?php echo $id; ?>">
         <!-- placeholder for Elements -->
-        <div id="card-element"></div>
+        <div id="card-element" class="card-wrapper"></div>
         <!-- Used to display form errors. -->
         <div id="card-errors" role="alert"></div>
-        <button id="card-button" data-secret="<?= $intent->client_secret ?>">
+
+        <button id="card-button" class="payment-submit-button" data-secret="<?= $intent->client_secret ?>">
           Submit Payment
         </button>
     </form>
+  </div>
 </section>
 
 

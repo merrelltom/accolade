585be15b63351768d043ccef65e33882a35c6200
Tom Merrell <tom@merrelltom.com>
2019-09-05 12:25:29 +0100

    Password protect all pages

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,6 +9,7 @@
   <?= css(['assets/css/style-1.0.css', '@auto']) ?>
   <script src="https://js.stripe.com/v3/"></script>
 </head>
+<?php if (!$kirby->user()) go('/') ?>
 <body data-large="<?= $site->large_trophy_price();?>" data-medium="<?= $site->medium_trophy_price();?>" data-small="<?= $site->small_trophy_price();?>"
       data-l-mod="<?= $site->small_trophy_variance();?>" data-m-mod="<?= $site->medium_trophy_variance();?>" data-s-mod="<?= $site->large_trophy_variance();?>"
       data-s-grad1="<?= $site->small_gradient_1();?>" data-s-grad2="<?= $site->small_gradient_2();?>" data-s-grad3="<?= $site->small_gradient_3();?>" data-s-grad4="<?= $site->small_gradient_4();?>"
